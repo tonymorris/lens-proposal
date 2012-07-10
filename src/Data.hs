a25ceadd131d41f016f0f3213ca67d1d2c18c0f7
Tony Morris <tmorris@tmorris.net>
2012-07-10 17:48:21 +1000

    kleisl

--- src/Data.hs
+++ src/Data.hs
@@ -3,6 +3,7 @@ module Data (module X) where
 import Prelude()
 import Data.Id as X
 import Data.Iso as X
+import Data.Kleisli as X
 import Data.Lens as X
 import Data.LabelledLens as X
 import Data.LabelledIso as X
